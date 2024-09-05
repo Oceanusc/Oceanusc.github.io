7d4a753d61e8f8b5327000343b2dec2458e175ea
Oceanusc <che4ac@163.com>
2024-09-05 15:01:25 +0800

    first commit

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,23 @@
+import { defineUserConfig } from "vuepress";
+import { getDirname, path } from "vuepress/utils";
+
+const __dirname = getDirname(import.meta.url);
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "博客演示",
+  description: "vuepress-theme-hope 的博客演示",
+
+  theme,
+  alias: {
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue",
+    ),
+  },
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
