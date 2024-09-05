fd2167be3278d8ab7f9b67f7343dfa3af6d94e62
cy <che4ac@163.com>
2024-09-05 17:24:29 +0800

    feat

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import { getDirname, path } from "vuepress/utils";
-
+import { oml2dPlugin } from 'vuepress-plugin-oh-my-live2d';
 const __dirname = getDirname(import.meta.url);
 import theme from "./theme.js";
 
@@ -18,6 +18,31 @@ export default defineUserConfig({
       "./components/BlogHero.vue",
     ),
   },
+  plugins: [
+    oml2dPlugin({
+      // 在这里配置选项
+      models: [
+        {
+          "path": "https://model.oml2d.com/HK416-1-normal/model.json",
+          "position": [0, 60],
+          "scale": 0.08,
+          "stageStyle": {
+            "height": 450
+          }
+        },
+        {
+          "path": "https://model.oml2d.com/cat-black/model.json",
+          "scale": 0.15,
+          "position": [0, 20],
+          "stageStyle": {
+            "height": 350
+          }
+        }
+      ]
+    })
+
+    //  ...other plugins
+  ]
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
