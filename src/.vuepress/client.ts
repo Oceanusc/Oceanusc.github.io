eaf3460c236c613bd584c5e35ff2b73f3a0b807d
cy <che4ac@163.com>
2024-09-09 18:00:25 +0800

    feat

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,6 +1,8 @@
 import { onMounted } from "vue";
 import { defineClientConfig } from "vuepress/client";
-
+import "vuepress-theme-hope/presets/round-blogger-avatar.scss";
+import "vuepress-theme-hope/presets/bounce-icon.scss";
+import "vuepress-theme-hope/presets/hr-driving-car.scss";
 export default defineClientConfig({
   setup() {},
 });
